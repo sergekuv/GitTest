40bdfa0e544bbcf5669794c88d401912d1021f84
SergeyKuvakin <sergekuv@gmail.com>
2023-09-02 09:00:28 +0300

    new

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, The Brave New World!");
         }
     }
 }
